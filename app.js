95ea8eb8dbd0692d5163199fd7330ae602671fc8
Espinha <90485846+Spnh@users.noreply.github.com>
2023-10-08 23:36:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ window.onload=function(){
 	cardJanela=document.querySelector(".imgCardJanela");
 	imgJanela=document.querySelector(".imgJanela");
 	carrega=document.querySelector(".lds-ring");
+
 	imgCard=document.querySelector(".imgCardJanela");
+	imgCard2=document.querySelector(".imgCardJanela2"); /* aqui */
+	imgCard3=document.querySelector(".imgCardJanela3"); /* aqui */
+
+	imgCard2.style.display="none"; 
+	imgCard3.style.display="none";
+
 	cardJanela.style.display="none";
 	janelaPrincipal.style.display="block";
 	janela.style.display="none";
 	
 	console.log(janela);
 	
-	return[janelaPrincipal,janela,carrega,imgCard,imgJanela]
+	return[janelaPrincipal,janela,carrega,imgCard,imgCard2,imgCard3,imgJanela]/* aqui */
 }
 function verificaCod(){ 
 	let cod=(document.querySelector(".cod").value)
@@ -24,7 +31,7 @@ function verificaCod(){
 		janela.style.display="block";
 		cod="1";
 		
-		setInterval(mudaImagem, 4000,cod)
+		setInterval(mudaImagem, 2000,cod)
 	}else if(cod=="NÃOAGUENTOMAIS"||cod=="NAOAGUENTOMAIS"||cod=="NÃO AGUENTO MAIS"||cod=="NAO AGUENTO MAIS"){
 		console.log(2)
 		janelaPrincipal.style.display="none";
@@ -32,7 +39,7 @@ function verificaCod(){
 		imgJanela.style.backgroundSize="cover"
 		janela.style.display="block";
 		cod="2";
-		setInterval(mudaImagem, 4000,cod)
+		setInterval(mudaImagem, 2000,cod)
 
 	}else if(cod=="SOU LINDA DEMAIS PRA ISSO"||cod=="SOULINDADEMAISPRAISSO"){
 		console.log(3)
@@ -40,7 +47,7 @@ function verificaCod(){
 		imgJanela.style.backgroundSize="cover"
 		janela.style.display="block";
 		cod="3";
-		setInterval(mudaImagem, 4000,cod)
+		setInterval(mudaImagem, 2000,cod)
 		imgJanela.style.backgroundImage="url('sharpey.jpeg')"
 	}else{
 		alert("BURRO(A) DO CARALHO, ESCREVE DIREITO LERDÃO(NA)")
@@ -55,10 +62,15 @@ function mudaImagem(codigo){
 		imgCard.src="luau.jpeg"
 	}else if(codigo=="2"){
 		imgCard.src="bar.jpeg"
+		imgCard2.style.display="block";
+		imgCard3.style.display="block";
 	}else if(codigo=="3"){
 		imgCard.src="piquenique.jpeg"
+		imgCard2.style.display="block";
 	}
 	carrega.style.display="none"
 	janela.style.display="none"
 	cardJanela.style.display="block"
+
+
 }
